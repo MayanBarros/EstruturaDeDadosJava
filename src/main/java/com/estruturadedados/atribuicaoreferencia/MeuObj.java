9bb4377f7885a7c171cd414ec83025974120d496
MAYAN BARROS DA SILVA <mayanbarrosdasilva@gmail.com>
2021-07-27 14:20:08 -0300

    Atribuição por Valor/Referencia!

---
+++ src/main/java/com/estruturadedados/atribuicaoreferencia/MeuObj.java
@@ -0,0 +1,19 @@
+package com.estruturadedados.atribuicaoreferencia;
+
+public class MeuObj {
+
+    public MeuObj(Integer num) {
+        this.num = num;
+    }
+
+    Integer num;
+
+    public  void setNum(Integer num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return this.num.toString();
+    }
+}
